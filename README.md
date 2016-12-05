02c07ebe6afde46b287f871853acce91b4de5fc1
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2016-12-05 20:10:33 +0100

    Import source tree

---
+++ README.md
@@ -0,0 +1,7 @@
+# Android AudioRecord Sample
+
+This is a sample project that demonstrates how to record audio on an Android device using the [`AudioRecord`](https://developer.android.com/reference/android/media/AudioRecord.html) class.
+
+## License
+
+This software is licensed under the terms of the Apache License, Version 2.0.
---
+++ src/main/java/com/example/audiorecord/AudioRecordActivity.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.audiorecord;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Sample that demonstrates how to record a device's microphone using {@link AudioRecord}.
+ */
+public class AudioRecordActivity extends AppCompatActivity {
+
+    private static final int SAMPLING_RATE_IN_HZ = 44100;
+
+    private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+
+    private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+
+    /**
+     * Factor by that the minimum buffer size is multiplied. The bigger the factor is the less
+     * likely it is that samples will be dropped, but more memory will be used. The minimum buffer
+     * size is determined by {@link AudioRecord#getMinBufferSize(int, int, int)} and depends on the
+     * recording settings.
+     */
+    private static final int BUFFER_SIZE_FACTOR = 2;
+
+    /**
+     * Size of the buffer where the audio data is stored by Android
+     */
+    private static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLING_RATE_IN_HZ,
+            CHANNEL_CONFIG, AUDIO_FORMAT) * BUFFER_SIZE_FACTOR;
+
+    /**
+     * Signals whether a recording is in progress (true) or not (false).
+     */
+    private final AtomicBoolean recordingInProgress = new AtomicBoolean(false);
+
+    private AudioRecord recorder = null;
+
+    private Thread recordingThread = null;
+
+    private Button startButton;
+
+    private Button stopButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        startButton = (Button) findViewById(R.id.btnStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecording();
+                startButton.setEnabled(false);
+                stopButton.setEnabled(true);
+            }
+        });
+
+        stopButton = (Button) findViewById(R.id.btnStop);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRecording();
+                startButton.setEnabled(true);
+                stopButton.setEnabled(false);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        stopRecording();
+    }
+
+    private void startRecording() {
+        recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLING_RATE_IN_HZ,
+                CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
+
+        recorder.startRecording();
+
+        recordingInProgress.set(true);
+
+        recordingThread = new Thread(new RecordingRunnable(), "Recording Thread");
+        recordingThread.start();
+    }
+
+    private void stopRecording() {
+        if (null == recorder) {
+            return;
+        }
+
+        recordingInProgress.set(false);
+
+        recorder.stop();
+
+        recorder.release();
+
+        recorder = null;
+
+        recordingThread = null;
+    }
+
+    private class RecordingRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            final File file = new File(Environment.getExternalStorageDirectory(), "recording.pcm");
+            final ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+
+            try (final FileOutputStream outStream = new FileOutputStream(file)) {
+                while (recordingInProgress.get()) {
+                    recorder.read(buffer, BUFFER_SIZE);
+                    outStream.write(buffer.array(), 0, BUFFER_SIZE);
+                    buffer.clear();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Writing of recorded audio failed", e);
+            }
+        }
+    }
+}
